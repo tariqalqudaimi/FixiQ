3a1e30ce80db1cbe83f6a3d2dc92641b42774d06
malikni3 <mniaz1096@gmail.com>
2025-11-01 18:16:58 +0300

    .

--- index.php
+++ index.php
@@ -432,10 +432,7 @@ if ($about_query) {
     </svg>
   </div>
 
-
-
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
-
   <script src="assets/vendor/purecounter/purecounter.js"></script>
   <script src="assets/vendor/aos/aos.js"></script>
   <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
